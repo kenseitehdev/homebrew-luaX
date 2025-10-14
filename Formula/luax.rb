fba8289f007850db473f550b5b24ed0a256a080d
Kensei <tehkensei@gmail.com>
2025-10-14 12:52:34 -0500

    updated formula for v1.0.1

--- Formula/luax.rb
+++ Formula/luax.rb
@@ -6,10 +6,10 @@ class Luax < Formula
   license "MIT"
   
   if OS.mac?
-    url "https://github.com/kenseitehdev/luaX/releases/download/v1.0.1/luaX_MacOS_ARM"
+    url "https://github.com/kenseitehdev/luaX/releases/download/1.0.1/luaX_MacOS_ARM"
     sha256 "a109e3441b6c1c37a3c659425723a320645cc185a0babf81da563edb2b681544"
   elsif OS.linux?
-    url "https://github.com/kenseitehdev/luaX/releases/download/v1.0.1/luaX_Nix_x64"
+    url "https://github.com/kenseitehdev/luaX/releases/download/1.0.1/luaX_Nix_x64"
     sha256 "7d878be3fc3d3b39867c017edc52a8da3e61e917b231ef96aa9436c122d33574"
   end
   
