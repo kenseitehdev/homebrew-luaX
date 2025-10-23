4b212c13618820e109d32b9eb2781a8bdcfc29d1
Kensei <tehkensei@gmail.com>
2025-10-22 21:47:23 -0500

    Update luaX to 1.0.2

--- Formula/luax.rb
+++ Formula/luax.rb
@@ -1,28 +1,27 @@
 class Luax < Formula
   desc "Extended Lua programming language"
   homepage "https://github.com/kenseitehdev/luaX"
-  version "1.0.1"
+  version "1.0.2"
   license "MIT"
-  
+
   if OS.mac?
-    url "https://github.com/kenseitehdev/luaX/releases/download/1.0.1/luaX_MacOS_ARM"
-    sha256 "a109e3441b6c1c37a3c659425723a320645cc185a0babf81da563edb2b681544"
+    url "https://github.com/kenseitehdev/luaX/releases/download/1.0.2/luaX_MacOS_ARM"
+    sha256 "fb7976ee096a54e9b94583c431c7385507b720e0fb626a0abfbb7fe067a5b3c8"
   elsif OS.linux?
-    url "https://github.com/kenseitehdev/luaX/releases/download/1.0.1/luaX_Nix_x64"
-    sha256 "7d878be3fc3d3b39867c017edc52a8da3e61e917b231ef96aa9436c122d33574"
+    url "https://github.com/kenseitehdev/luaX/releases/download/1.0.2/luaX_Nix_x64"
+    sha256 "52994088d055ddd1b8882d0e2cc3ce8da1b885488bd797535dd518b09f20d24c"
   end
-  
+
   def install
     if OS.mac?
       bin.install "luaX_MacOS_ARM" => "luax"
     elsif OS.linux?
       bin.install "luaX_Nix_x64" => "luax"
     end
-    
     chmod 0755, bin/"luax"
   end
-  
+
   test do
-    assert_match "1.0.1", shell_output("#{bin}/luax --version")
+    assert_match "1.0.2", shell_output("#{bin}/luax --version")
   end
 end
